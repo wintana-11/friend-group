d20f8687e2f759e152cbe262d89db1abdaef7bba
Amaarah Simjee <ucapas9@ucl.ac.uk>
2023-10-19 10:24:44 +0100

    added friend groups

--- group.py
+++ group.py
@@ -1,5 +1,10 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
 
-my_group =
+friend1 = {'name': 'Jill', 'age': 26, 'job': 'Biologist', 'friends':'Zalika', 'partner': 'John', 'cousin': None, 'landlord': None }
+friend2 = {'name': 'Zalika', 'age': 28, 'job': 'Artist', 'friends':'Jill', 'partner': 'John', 'cousin': None, 'landlord': None }
+friend3 = {'name': 'John', 'age': 27, 'job': 'Writer', 'friends':None , 'partner': 'Jill', 'cousin': None, 'landlord': None }
+friend4 = {'name': 'Nash', 'age': 34, 'job': 'Chef', 'friends': None, 'partner': None, 'cousin': 'John', 'landlord': None }
+
+my_group = [friend1, friend2, friend3, friend4]
+
